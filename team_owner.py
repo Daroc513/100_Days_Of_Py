79d1b1f0ed5dbbdf77be69a8761d150ebd775a61
Daroc513 <rakeemembrose@gmail.com>
2024-01-25 20:00:55 -0500

    first commit

---
+++ team_owner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Creates a greeting.
+name = input("What is your name?\n")
+print("Hello " + name  + " I've been waiting for you.")
+sport =  input ("Whats your favorite sport?\n")
+# Asks the user for the city that they grew up in.
+city = input("What city are you from?\n")
+#Asks the user for the name of a pet.
+pet = input("What was the name of your favorite pet?\n")
+# Creats the users sports team
+print("You should own a " + sport + " team. Yea, name it the "+ city + " " + pet + "s")
